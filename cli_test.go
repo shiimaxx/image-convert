40d5b637d3160f4aa215ff1cf919c163ff4a7045
shiimaxx <yoshima@shiimaxx.com>
2018-04-22 19:26:02 +0900

    Add TestRun_ImageConvert

--- cli_test.go
+++ cli_test.go
@@ -9,6 +9,50 @@ import (
 	"testing"
 )
 
+var cases = []struct {
+	args    string
+	outputs []string
+}{
+	{
+		args: "image-convert testdata",
+		outputs: []string{
+			"testdata/icon-001.png",
+			"testdata/dir-001/icon-002.png",
+			"testdata/dir-002/icon-003.png",
+			"testdata/dir-002/dir-002-001/icon-004.png",
+		},
+	},
+	{
+		args: "image-convert -s jpg -d gif testdata",
+		outputs: []string{
+			"testdata/icon-001.gif",
+			"testdata/dir-001/icon-002.gif",
+			"testdata/dir-002/icon-003.gif",
+			"testdata/dir-002/dir-002-001/icon-004.gif",
+		},
+	},
+}
+
+func TestRun_ImageConvert(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+
+	for _, c := range cases {
+		args := strings.Split(c.args, " ")
+		status := cli.Run(args)
+		if status != ExitCodeOK {
+			t.Errorf("expected %d to eq %d", status, ExitCodeOK)
+		}
+		for _, o := range c.outputs {
+			_, err := os.Stat(o)
+			if os.IsNotExist(err) {
+				t.Error(err)
+			}
+		}
+	}
+
+}
+
 func TestRun_versionFlag(t *testing.T) {
 	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
 	cli := &CLI{outStream: outStream, errStream: errStream}
